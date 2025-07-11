c9ee1900acdad6ad51d1b0b669d07a49745e3ff4
Tushar25yadav <2599tusharyadav@gmail.com>
2025-07-11 18:08:49 +0530

    remove motion

--- app/page.tsx
+++ app/page.tsx
@@ -8,25 +8,9 @@ export default function Home() {
   return (
     <>
       <Nav />
-      <motion.div
-        initial={{ scale: 1.1, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        transition={{ duration: 3, ease: 'easeIn' }}
-        className="hero-background"
-      >
-        <div
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            height: '100%',
-            width: '100%',
-            backgroundColor: 'rgba(0, 0, 0, 0.6)', // adjust 0.4 for more/less darkness
-            zIndex: 1,
-          }}
-        >
-        </div>
-      </motion.div>
+      <div className="background-section">
+        <div className="overlay"></div>
+      </div>
       <Sample />
     </>
   );
